e211c9fb1586880dd83d512ede06b76eb75b619d
caronrispat-max <caronrispat@gmail.com>
2026-05-22 22:51:33 +0300

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting Tweet Pipeline Pro License Server...
-echo Open http://127.0.0.1:8000/admin to configure it.
-python -m uvicorn license_server:app --host 0.0.0.0 --port 8000
-pause
